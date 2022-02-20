69298aaa25bcfca2d085ad5607fd956908323e76
Timo <timo.schlegel@campus.tu-berlin.de>
2022-02-20 14:39:06 +0100

    parsing works

--- keyboard.py
+++ keyboard.py
@@ -1,3 +1,4 @@
+from audioop import add
 import xml.etree.ElementTree as ET
 class Key:
     def __init__(self,row:int) -> None:
@@ -11,27 +12,74 @@ class Key:
         self.symbols    = []
         self.row        = row
     def parse(self, element):
-        self.id         = element.find("id").text
-        self.position   = element.find("position").text
-        self.width      = element.find("width").text
+        self.id         = int(element.find("id").text)
+        self.position   = int(element.find("position").text)
+        self.width      = float(element.find("width").text)
         self.hand       = element.find("hand").text
         self.finger     = element.find("finger").text
-        self.effort     = element.find("effort").text
+        self.effort     = float(element.find("effort").text)
+    def layoutString(self, layer = "id"):
+        s = "|"
+        for i in range(int(self.width/0.25)):
+            s += " "
+        if layer == "id":
+            s += f"{self.id:4}"
+        else:
+            try:
+                s+= f"{list(filter(lambda x: x.layerName == layer, self.symbols))[0].value:4}"
+            except:
+                s+= "    "
+        for i in range(int(self.width/0.25)):
+            s += " "
+        s += "|"
+        return s
 
 class Keyboard:
     def __init__(self,name) -> None:
         self.name = name
-        self.keys = {}
-    def addKey(self,key:Key):
-        self.keys[key.id]=Key
+        self.keys = []
+        self.symbols = []
 
-class Symbol:
-    def __init__(self, value:str, layerEffort:float, keyID:int, additionalEffort:float=1.0) -> None:
-        self.value = value
-        self.layerEffort = layerEffort
-        self.keyID = keyID
-        self.additionalEffort = additionalEffort
-        self.key = None
+    def addKey(self,key:Key):
+        if not key in self.keys:
+            self.keys.append(key)
     
-    def addKey(self, keys):
-        self.key = keys[self.keyID]
+    def layoutString(self,layer="id"):
+        s = f"Keybord name: {self.name}\n"
+        s += f"Layer: {layer}\n"
+        s += "Keys: \n"
+        minRow, maxRow = self.getMinMaxRow()
+        for rowID in range(maxRow,minRow-1,-1):
+            row = self.getRow(rowID)
+            for k in row:
+                s+=k.layoutString(layer=layer)
+            s += "\n"
+        return s
+    def getMinMaxRow(self):
+        keysSorted = list(sorted(self.keys, key=lambda x: x.row,reverse=True))
+        return keysSorted[-1].row, keysSorted[0].row
+
+    def getRow(self, rowID):
+        return list(sorted(filter(lambda x: x.row == rowID,self.keys),key=lambda x: x.position))
+
+    def getKey(self, keyID):
+        try:
+            return list(filter(lambda x: x.id==keyID, self.keys))[0]
+        except:
+            return None
+    
+    
+class Symbol:
+    def __init__(self) -> None:
+        self.value = ""
+        self.layerEffort = 1.0
+        self.key = None
+        self.additionalEffort = 1.0
+        self.layerName = ""
+
+    def parse(self, element):
+        self.value = element.attrib["value"]
+        try: 
+            self.additionalEffort = element.attrib["additionalEffort"]
+        except:
+            self.additionalEffort = 1.0    
--- parsing.py
+++ parsing.py
@@ -3,6 +3,7 @@ from keyboard import Key, Symbol, Keyboard
 import xml.etree.ElementTree as ET
 
 def parseLayout(layoutName="qwertz",pathToLayouts="layouts.xml"):
+    symbols = []
     layouts = ET.parse(pathToLayouts).getroot().findall("layout")
     layout = None
     for l in layouts:
@@ -13,6 +14,24 @@ def parseLayout(layoutName="qwertz",pathToLayouts="layouts.xml"):
         raise Exception("No such layout")
     print(layout,layout.attrib)
     kb = parseBoard(layout.attrib["keyboard"])
+
+    for layer in layout.findall("layer"):
+        layerName=layer.attrib["layer_name"]
+        print(f"Found Layer: {layerName}")
+        layerEffort = layer.attrib["effort"]
+        print("effort: ",layerEffort)
+        for symbol in layer.findall("symbol"):
+            #print(symbol.attrib["value"])
+            key = kb.getKey(int(symbol.attrib["key"]))
+            sym = Symbol()
+            sym.layerEffort = layerEffort
+            sym.layerName= layerName
+            sym.parse(symbol)
+            if key != None:
+                key.symbols.append(sym)
+                sym.key = key
+    kb.symbols = symbols
+    return kb
 
 def parseBoard(keyboardName="laptop",pathToKeyboards="keyboards.xml"):
     keyboards = ET.parse(pathToKeyboards).getroot().findall('keyboard')
@@ -27,11 +46,15 @@ def parseBoard(keyboardName="laptop",pathToKeyboards="keyboards.xml"):
     kb = Keyboard(keyboardName)
     for r in keyboard.findall("row"):
         for k in r.findall("key"):
-            key = Key(row= r.attrib["position"])
+            key = Key(row= int(r.attrib["position"]))
             key.parse(k)
+            kb.addKey(key)
+    return kb
             
 def main():
-    parseLayout("qwertz")
+    kb = parseLayout("qwertz")
+    print(kb.layoutString("shift"))
+    
 
 if __name__ == "__main__":
     main()
